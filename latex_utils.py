1081c8b87b55aa463d6bdc184720874aa24a8921
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2026-07-03 22:26:43 -0300

    refactor: simplificar função de escape de LaTeX

--- latex_utils.py
+++ latex_utils.py
@@ -1,23 +1,26 @@
-import re
+# Caracteres com significado especial no LaTeX. A ordem importa: a barra
+# invertida precisa ser tratada primeiro para não escapar duplicado o que
+# as outras substituições geram.
+_LATEX_ESCAPE_MAP = {
+    '\\': r'\textbackslash{}',
+    '{': r'\{',
+    '}': r'\}',
+    '$': r'\$',
+    '&': r'\&',
+    '#': r'\#',
+    '_': r'\_',
+    '%': r'\%',
+    '~': r'\textasciitilde{}',
+    '^': r'\textasciicircum{}',
+}
 
 
 def escapar_latex(texto):
+    """Escapa caracteres especiais do LaTeX em texto vindo do usuário.
+
+    Sem isso, valores como 'P&D', '100%' ou 'C#' quebram a compilação do
+    PDF (ou, em teoria, poderiam injetar comandos LaTeX arbitrários).
+    """
     if texto is None:
-        return ""
-
-    s = str(texto)
-    substituicoes = {
-        '\\': r'\textbackslash{}',
-        '&': r'\&',
-        '%': r'\%',
-        '$': r'\$',
-        '#': r'\#',
-        '_': r'\_',
-        '{': r'\{',
-        '}': r'\}',
-        '~': r'\textasciitilde{}',
-        '^': r'\textasciicircum{}',
-    }
-
-    padrao = re.compile(r'([\\&%$#_{}~^])')
-    return padrao.sub(lambda m: substituicoes[m.group(1)], s)
+        return ''
+    return ''.join(_LATEX_ESCAPE_MAP.get(ch, ch) for ch in str(texto))
